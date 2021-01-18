9bafb3c24a032191811c4c8255ef93642460d4a5
zenraiching <rickdavidsangalang@gmail.com>
2021-01-18 15:46:14 +0800

    add

---
+++ calcu.py
@@ -0,0 +1,11 @@
+from math import sin, cos,tan, asin, atan, acos, degrees, pi, radians
+inputSide_X = "" 
+inputSide_Y = "looking"
+inputSide_R = 36
+
+inputAngle_X = ""
+inputAngle_Y = 60
+inputAngle_R = ""
+
+degree= (radians(inputAngle_Y))
+print(sin(degree))
