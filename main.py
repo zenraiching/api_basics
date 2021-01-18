2fb626af2de4c513265a3af351cec75184189be6
zenraiching <rickdavidsangalang@gmail.com>
2021-01-18 21:47:20 +0800

    add

---
+++ main.py
@@ -0,0 +1,9 @@
+def chooseTriangle():
+    import sovlingTriangle
+def chooseAngle():
+    import angleCalcu
+choice = input("Enter choice")
+if choice == "a":
+    chooseTriangle()
+if choice =="b":
+    chooseAngle()
